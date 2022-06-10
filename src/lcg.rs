438fcd876d4322c04a783494264810101ae58640
Alan Fernandez <alanf718@gmail.com>
2022-06-09 22:33:11 -0400

    removing unused comments

--- src/lcg.rs
+++ src/lcg.rs
@@ -15,19 +15,7 @@ const DEFAULT_LCG_PARAMS: LCGParams = LCGParams {
 	x0: 0
 };
 
-// 	const seed: Seed = new Seed({
-// 		...defaultParams,
-// 		x0: n,
-// 	});
-// 	const c = 2 * seed.randInt(1, 3000) + 1;
-// 	const a = 4 * seed.randInt(3000, 6000) + 1;
-// 	const m = Math.pow(2, 32);
-// 	const x0 = seed.randInt(1, m);
-// 	return {
-// 		c, a, m, x0,
-// 	};
 pub fn number_to_params(n: u32) -> LCGParams {
-	// let seed: Seed = crate::seed::Seed::new(DEFAULT_LCG_PARAMS);
 	let mut s = Seed::new(DEFAULT_LCG_PARAMS, n);
 	let m = u32::pow(2, 31);
 	LCGParams {
@@ -37,30 +25,3 @@ pub fn number_to_params(n: u32) -> LCGParams {
 		x0: s.rand_uint(1, m)
 	}
 }
-// export interface LCGParams {
-// 	c: number;
-// 	a: number;
-// 	m: number;
-// 	x0: number;
-// }
-
-// const defaultParams: LCGParams = {
-// 	c: 2 * 117 + 1,
-// 	a: 4 * 317 + 1,
-// 	m: Math.pow(2, 16),
-// 	x0: 0,
-// };
-
-// export const numberToParams = (n: number): LCGParams => {
-// 	const seed: Seed = new Seed({
-// 		...defaultParams,
-// 		x0: n,
-// 	});
-// 	const c = 2 * seed.randInt(1, 3000) + 1;
-// 	const a = 4 * seed.randInt(3000, 6000) + 1;
-// 	const m = Math.pow(2, 32);
-// 	const x0 = seed.randInt(1, m);
-// 	return {
-// 		c, a, m, x0,
-// 	};
-// };
